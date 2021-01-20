a7393df8e9131e7aeb788fb04ee0c3d381e5635f
cxan96 <c.p.xanten@gmail.com>
2021-01-20 18:44:13 +0100

    added license_plate_extraction package

---
+++ src/license_plate_extraction/data_reader.py
@@ -0,0 +1,6 @@
+from license_plate_extraction import settings
+import numpy as np
+
+
+def get_bounding_box_from_xml_path(path) -> np.ndarray:
+    pass
---
+++ tests/license_plate_extraction/test_data_reader.py
@@ -0,0 +1,5 @@
+from license_plate_extraction import data_reader
+
+
+def test_abc():
+    assert 1 == 1
