16fe8eba2434b520b1207c93759e33d30e11ae72
cxan96 <c.p.xanten@gmail.com>
2021-01-20 21:58:28 +0100

    implemented bounding box reader and added testcases

--- src/license_plate_extraction/data_reader.py
+++ src/license_plate_extraction/data_reader.py
@@ -1,6 +1,19 @@
 from license_plate_extraction import settings
 import numpy as np
+from bs4 import BeautifulSoup
+from pathlib import Path
 
 
-def get_bounding_box_from_xml_path(path) -> np.ndarray:
-    pass
+def get_bounding_box_from_xml_path(path: Path) -> np.ndarray:
+    """
+    Returns np.array([x_min, y_min, x_max, y_max])
+    """
+    with open(path, mode="r") as file:
+        bs = BeautifulSoup(file, "xml")
+
+    x_min = float(bs.bndbox.xmin.string)
+    x_max = float(bs.bndbox.xmax.string)
+    y_min = float(bs.bndbox.ymin.string)
+    y_max = float(bs.bndbox.ymax.string)
+
+    return np.array([x_min, y_min, x_max, y_max])
--- tests/license_plate_extraction/test_data_reader.py
+++ tests/license_plate_extraction/test_data_reader.py
@@ -1,5 +1,46 @@
-from license_plate_extraction import data_reader
+from license_plate_extraction import data_reader, settings
+import numpy as np
 
 
-def test_abc():
-    assert 1 == 1
+def test_read_eu_bounding_box():
+    xml_path = settings.DATA_DIR / "eu_cars+lps" / "1T43213_car_eu.xml"
+
+    result = data_reader.get_bounding_box_from_xml_path(xml_path)
+
+    assert np.isclose(result[0], 213) == True
+    assert np.isclose(result[1], 214) == True
+    assert np.isclose(result[2], 314) == True
+    assert np.isclose(result[3], 237) == True
+
+
+def test_read_br_bounding_box():
+    xml_path = settings.DATA_DIR / "br_cars+lps" / "AYO9034_car_br.xml"
+
+    result = data_reader.get_bounding_box_from_xml_path(xml_path)
+
+    assert np.isclose(result[0], 528) == True
+    assert np.isclose(result[1], 412) == True
+    assert np.isclose(result[2], 690) == True
+    assert np.isclose(result[3], 464) == True
+
+
+def test_read_ro_bounding_box():
+    xml_path = settings.DATA_DIR / "ro_cars+lps" / "1912BG14_car_eu.xml"
+
+    result = data_reader.get_bounding_box_from_xml_path(xml_path)
+
+    assert np.isclose(result[0], 1400) == True
+    assert np.isclose(result[1], 1144) == True
+    assert np.isclose(result[2], 1875) == True
+    assert np.isclose(result[3], 1293) == True
+
+
+def test_read_us_bounding_box():
+    xml_path = settings.DATA_DIR / "us_cars+lps" / "0SG719_car_us.xml"
+
+    result = data_reader.get_bounding_box_from_xml_path(xml_path)
+
+    assert np.isclose(result[0], 911) == True
+    assert np.isclose(result[1], 136) == True
+    assert np.isclose(result[2], 973) == True
+    assert np.isclose(result[3], 167) == True
