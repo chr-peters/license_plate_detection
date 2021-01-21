8e782331c787d19b613370f48e41d969385bbdf2
cxan96 <c.p.xanten@gmail.com>
2021-01-21 19:15:03 +0100

    Read images using tensorflow

--- src/license_plate_extraction/data_reader.py
+++ src/license_plate_extraction/data_reader.py
@@ -1,7 +1,9 @@
 from license_plate_extraction import settings
+from license_plate_extraction.visualization_tools import show_image
 import numpy as np
 from bs4 import BeautifulSoup
 from pathlib import Path
+import tensorflow as tf
 
 
 def get_bounding_box_from_xml_path(path: Path) -> np.ndarray:
@@ -17,3 +19,27 @@ def get_bounding_box_from_xml_path(path: Path) -> np.ndarray:
     y_max = float(bs.bndbox.ymax.string)
 
     return np.array([x_min, y_min, x_max, y_max])
+
+
+def read_image_as_tensor(path: Path) -> tf.Tensor:
+    """
+    Returns a 3D Tensor object representing the image as RGB.
+
+    Details here:
+    https://www.tensorflow.org/tutorials/load_data/images#using_tfdata_for_finer_control
+    """
+    img_data = tf.io.read_file(str(path))
+    img_tensor = tf.io.decode_jpeg(img_data, channels=3)
+
+    return img_tensor
+
+
+if __name__ == "__main__":
+    sample_path = settings.DATA_DIR / "eu_cars+lps" / "1T43213_car_eu.jpg"
+    bounding_box_path = settings.DATA_DIR / "eu_cars+lps" / "1T43213_car_eu.xml"
+
+    img_tensor = read_image_as_tensor(sample_path)
+
+    bounding_box = get_bounding_box_from_xml_path(bounding_box_path)
+
+    show_image(img_tensor, bounding_box)
---
+++ src/license_plate_extraction/visualization_tools.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+def show_image(img, bounding_box=None):
+    fig, ax = plt.subplots()
+
+    ax.imshow(img)
+
+    if bounding_box is not None:
+        x_min, y_min, x_max, y_max = bounding_box
+
+        width = x_max - x_min
+        height = y_max - y_min
+
+        rectangle = patches.Rectangle(
+            xy=(x_min, y_min),
+            width=width,
+            height=height,
+            edgecolor="red",
+            facecolor="none",
+            linewidth=1,
+        )
+
+        ax.add_patch(rectangle)
+
+    plt.show()
