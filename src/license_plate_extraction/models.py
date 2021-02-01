d6e50c021602c16d5b6c6d30305ec96d13a512dc
AnneSophie1 <75671292+AnneSophie1@users.noreply.github.com>
2021-02-01 17:31:29 +0100

    neues modell

--- src/license_plate_extraction/models.py
+++ src/license_plate_extraction/models.py
@@ -52,8 +52,30 @@ def tiny_yolo_inspired(input_height, input_width, num_channels):
 
     return model
 
+def test_model(input_height, input_width, num_channels):
+    model = Sequential(
+        [
+            Input(shape=(input_height, input_width, num_channels)),
+            layers.experimental.preprocessing.Rescaling(
+                1.0 / 255,
+            ),
+            layers.Conv2D(16, 3, padding="same", activation="relu"),
+            layers.Conv2D(16, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dropout(0.2),
+            layers.Dense(4, activation="sigmoid", use_bias=False),
+        ]
+    )
+
+    return model
+
 
 if __name__ == "__main__":
-    model = tiny_yolo_inspired(448, 448, 3)
+    model = test_model(448, 448, 3)
 
     print(model.summary())
