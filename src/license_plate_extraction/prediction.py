b80ccb04aa370fcad5b5e9cb1b2a90f8406e6880
cxan96 <c.p.xanten@gmail.com>
2021-02-06 18:57:47 +0100

    prediction function with real model

--- src/license_plate_extraction/prediction.py
+++ src/license_plate_extraction/prediction.py
@@ -1,23 +1,39 @@
 from pathlib import Path
 import numpy as np
-from data_reader import (
-    get_bounding_box_from_xml_path,
-    get_bounding_box_xml_path_from_image_path,
-)
+import tensorflow as tf
+import data_reader
 import settings
 
 
-def predict_bounding_box(path: Path) -> np.ndarray:
+PREDICTION_MODEL_PATH = settings.MODELS_DIR / "test_01.tf"
+
+
+def predict_bounding_box(
+    image_path: Path, model_path=PREDICTION_MODEL_PATH
+) -> np.ndarray:
     """
     Predicts a bounding box for an image.
 
-    :param path: The path where the image is located.
+    :param image_path: The path where the image is located.
+    :param model_path: The path where the trained keras model is located.
     :returns: An np.ndarray containing the bounding box prediction in the format [x_min, y_min, width, height] (Pixel values).
     """
-    bounding_box_path = get_bounding_box_xml_path_from_image_path(path)
-    bounding_box = get_bounding_box_from_xml_path(bounding_box_path)
+    model = tf.keras.models.load_model(model_path)
 
-    return bounding_box
+    image_tensor = data_reader.read_image_as_tensor(image_path)
+
+    # resize image to match model input
+    input_height = model.inputs[0].shape[1]
+    input_width = model.inputs[0].shape[2]
+    image_tensor = tf.image.resize(
+        image_tensor, size=(input_height, input_width), antialias=True
+    )
+
+    image_tensor = tf.reshape(image_tensor, shape=(1, input_height, input_width, 3))
+
+    prediction = model.predict(image_tensor)
+
+    return prediction[0]
 
 
 if __name__ == "__main__":
