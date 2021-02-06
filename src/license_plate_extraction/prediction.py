891663d3ea30569aa2f9790bc258e1d637ea75e2
cxan96 <c.p.xanten@gmail.com>
2021-02-06 19:24:27 +0100

    prediction pipeline first version

--- src/license_plate_extraction/prediction.py
+++ src/license_plate_extraction/prediction.py
@@ -3,24 +3,23 @@ import numpy as np
 import tensorflow as tf
 import data_reader
 import settings
+import visualization_tools
 
 
 PREDICTION_MODEL_PATH = settings.MODELS_DIR / "test_01.tf"
 
 
 def predict_bounding_box(
-    image_path: Path, model_path=PREDICTION_MODEL_PATH
+    image_tensor: tf.Tensor, model_path=PREDICTION_MODEL_PATH
 ) -> np.ndarray:
     """
     Predicts a bounding box for an image.
 
-    :param image_path: The path where the image is located.
+    :param image_tensor: A tf.Tensor object containing the image. Shape: [height, width, 3]
     :param model_path: The path where the trained keras model is located.
-    :returns: An np.ndarray containing the bounding box prediction in the format [x_min, y_min, width, height] (Pixel values).
+    :returns: An np.ndarray containing the bounding box prediction in the format [x_min, y_min, width, height] (Percent values).
     """
     model = tf.keras.models.load_model(model_path)
-
-    image_tensor = data_reader.read_image_as_tensor(image_path)
 
     # resize image to match model input
     input_height = model.inputs[0].shape[1]
@@ -31,14 +30,6 @@ def predict_bounding_box(
 
     image_tensor = tf.reshape(image_tensor, shape=(1, input_height, input_width, 3))
 
-    prediction = model.predict(image_tensor)
+    prediction = model(image_tensor)[0]
 
-    return prediction[0]
-
-
-if __name__ == "__main__":
-    example_image_path = settings.DATA_DIR / "eu_cars+lps/1T43213_car_eu.jpg"
-
-    prediction = predict_bounding_box(example_image_path)
-
-    print(prediction)
+    return prediction.numpy()
