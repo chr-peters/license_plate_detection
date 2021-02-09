7d72369da9bce3d82e8e857203bb2cef8cb3530e
cxan96 <c.p.xanten@gmail.com>
2021-02-09 19:12:48 +0100

    command line tool now working

--- src/license_plate_detection.py
+++ src/license_plate_detection.py
@@ -21,9 +21,12 @@ from license_plate_extraction.visualization_tools import show_image
     help="Visualize the prediction using matplotlib.",
 )
 @click.argument("path", type=click.Path(exists=True, resolve_path=True))
-def main(visualize, path):
+@click.argument("outfile", type=click.File("w"), required=False)
+def main(visualize, path, outfile):
     """
     Print the license plate and it's bounding box of a car detected in an image.
+
+    The bounding box format is [x_min, y_min, width, height] in pixel values.
 
     PATH refers to the location of a JPEG image or a directory of JPEG images.
     If PATH is a directory, a file will be created that contains all the predicted license plates.
@@ -44,14 +47,24 @@ def main(visualize, path):
             )
 
     elif path.is_dir():
+        if outfile is None:
+            click.echo(
+                "Error: If PATH is a directory, OUTFILE must be specified! See license_plate_detection.py --help",
+                err=True,
+            )
+            return
+
         image_path_list = get_image_paths_from_directory(path)
         for cur_image_path in image_path_list:
             cur_bounding_box, cur_prediction = make_prediction(cur_image_path)
 
             cur_image_name = cur_image_path.stem
+            cur_line = f"{cur_image_name}:{cur_prediction}"
+
             click.echo(f"{cur_image_name}:{cur_prediction}")
+            outfile.write(cur_line + "\n")
     else:
-        click.echo("PATH must be either a file or a directory!", err="True")
+        click.echo("PATH must be either a file or a directory!", err=True)
 
 
 if __name__ == "__main__":
