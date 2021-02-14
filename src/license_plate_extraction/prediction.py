f031e8c214b697039a04299695ff3b88d2c7e020
AnneSophie1 <75671292+AnneSophie1@users.noreply.github.com>
2021-02-14 22:08:18 +0100

    new

--- src/license_plate_extraction/prediction.py
+++ src/license_plate_extraction/prediction.py
@@ -6,7 +6,7 @@ import settings
 import visualization_tools
 
 
-PREDICTION_MODEL_PATH = settings.MODELS_DIR / "tiny_best.tf"
+PREDICTION_MODEL_PATH = settings.MODELS_DIR / "tiny_02.tf"
 prediction_model = tf.keras.models.load_model(PREDICTION_MODEL_PATH)
 
 
