73a9af29c284dd02e1ec430ab263b98061ca5c6b
PiavK <75673320+PiavK@users.noreply.github.com>
2021-02-15 12:21:31 +0100

    New OCR_Pipeline

---
+++ src/character_recognition/ocr_functions_notWorking.py
@@ -0,0 +1,264 @@
+import cv2
+import numpy as np
+import pytesseract
+#from pathlib import Path
+import pandas as pd
+
+
+def extract_plate(img, bounding_box):
+    x = int(round(bounding_box[0] * img.shape[1]))
+    y = int(round(bounding_box[1] * img.shape[0]))
+    w = int(round(bounding_box[2] * img.shape[1]))
+    h = int(round(bounding_box[3] * img.shape[0]))
+
+    img_bound = img[y : y + h, x : x + w]
+    x_start = x
+    y_start = y
+    
+    return img_bound, x_start, y_start
+
+
+def extract_plate_hor(img, bounding_box, change, left):
+    x = int(round(bounding_box[0] * img.shape[1]))
+    y = int(round(bounding_box[1] * img.shape[0]))
+    w = int(round(bounding_box[2] * img.shape[1]))
+    h = int(round(bounding_box[3] * img.shape[0]))
+
+    if left:
+        img_bound = img[y : y + h, x - change : x + w - change]
+        x_start = x - change
+        y_start = y
+    else:
+        img_bound = img[y : y + h, x + change : x + w + change]
+        x_start = x + change
+        y_start = y 
+    
+    return img_bound, x_start, y_start
+
+
+def extract_plate_ver(img, bounding_box, change, up):
+    x = int(round(bounding_box[0] * img.shape[1]))
+    y = int(round(bounding_box[1] * img.shape[0]))
+    w = int(round(bounding_box[2] * img.shape[1]))
+    h = int(round(bounding_box[3] * img.shape[0]))
+
+    if up:
+        img_bound = img[y - change : y + h - change, x : x + w]
+        x_start = x
+        y_start = y - change
+    else:
+        img_bound = img[y + change: y + h + change, x : x + w]
+        x_start = x
+        y_start = y + change
+    
+    return img_bound, x_start, y_start
+
+
+def extract_plate_side(img, bounding_box, change_x, change_y, coords):
+    x = int(round(bounding_box[0] * img.shape[1]))
+    y = int(round(bounding_box[1] * img.shape[0]))
+    w = int(round(bounding_box[2] * img.shape[1]))
+    h = int(round(bounding_box[3] * img.shape[0]))
+
+    if coords == 1: # topright
+        img_bound = img[y - change_y : y + h - change_y, x + change_x : x + w + change_x]
+        x_start = x + change_x
+        y_start = y - change_y
+    if coords == 2: # bottomright
+        img_bound = img[y + change_y : y + h + change_y, x + change_x : x + w + change_x]
+        x_start = x + change_x
+        y_start = y + change_y
+    if coords == 3: # bottomleft
+        img_bound = img[y + change_y : y + h + change_y, x - change_x : x + w - change_x]
+        x_start = x - change_x
+        y_start = y + change_y
+    if coords == 4: # topleft
+        img_bound = img[y - change_y : y + h - change_y, x - change_x : x + w - change_x]
+        x_start = x - change_x
+        y_start = y - change_y
+    
+    return img_bound, x_start, y_start
+
+
+def image_preproc(img_bound):
+    gray = cv2.cvtColor(img_bound, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    blur_g = cv2.GaussianBlur(gray, (5, 5), 0)
+    blur = cv2.medianBlur(blur_g, 3)
+    ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    dilation = cv2.dilate(thresh, rect_kern, iterations=1)
+    opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, rect_kern)
+
+    contours, hierarchy = cv2.findContours(
+        opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
+    sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+    
+    return sorted_contours, gray, thresh
+
+
+def ocr_extraction(sorted_contours, gray, thresh, x_start, y_start):
+    data_plate = pd.DataFrame()
+    plate_num = ""
+    i = 1
+
+    for cnt in sorted_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        height, width = gray.shape        
+        if height / float(h) > 4:
+            continue
+        ratio = h / float(w)
+        if ratio < 1.2:
+            continue
+        if width / float(w) > 50:
+            continue  # 25
+        rect = cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi = thresh[y - 5 : y + h + 5, x - 5 : x + w + 5]
+        if roi.size == 0:
+            roi = thresh[y - 2 : y + h + 2, x - 2 : x + w + 2]
+        if roi.size == 0:
+            roi = thresh[y : y + h, x : x + w]
+        roi = cv2.bitwise_not(roi)
+        roi = cv2.medianBlur(roi, 5)        
+        dat = pytesseract.image_to_data(
+            roi,
+            config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3",
+            output_type="data.frame",
+        )
+        dat["number"] = i
+        dat["x"] = (x/3) + x_start
+        dat["y"] = (y/3) + y_start
+        dat["w"] = w
+        dat["h"] = h
+        i += 1
+        text = pytesseract.image_to_string(
+            roi,
+            config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3",
+        )
+        data_plate = pd.concat([data_plate, dat])
+        plate_num += text
+
+    data_plate = data_plate.dropna()
+    if not data_plate.empty: 
+        data_plate = data_plate.set_index("number")
+        data_plate = data_plate.drop(columns=['level', 'page_num', 'block_num', 
+                                              'par_num', 'line_num', 'word_num',
+                                              'left', 'top', 'width', 'height'])
+    plate_num = plate_num.splitlines()
+    plate_num = "".join(map(str, plate_num))
+
+    return data_plate, plate_num
+
+
+def ocr(img, bounding_box, method):
+    # check which method is used:
+    if method == "normal":
+        img_bound, x_start, y_start = extract_plate(img, bounding_box)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh, 
+                                               x_start, y_start)
+        
+    if method == "up":
+        img_bound_up, x_startup, y_startup = extract_plate_ver(img, bounding_box, 
+                                                    int(bounding_box[1]*img.shape[0]*0.1), 
+                                                    True)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh, 
+                                               x_start, y_start)
+    
+    if method == "down":
+        img_bound_down, x_startdown, y_startdown = extract_plate_ver(img, bounding_box, 
+                                                    int(bounding_box[1]*img.shape[0]*0.1),
+                                                    False)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh,
+                                               x_start, y_start)
+    
+    if method == "left":
+        img_bound_left, x_startleft, y_startleft = extract_plate_hor(img, bounding_box, 
+                                                    int(bounding_box[0]*img.shape[1]*0.1), 
+                                                    True)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh, 
+                                               x_start, y_start)
+    
+    if method == "right":
+        img_bound_right, x_startright, y_startright = extract_plate_hor(img, bounding_box, 
+                                                    int(bounding_box[0]*img.shape[1]*0.1), 
+                                                    False)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh,
+                                               x_start, y_start)
+
+    if method == "topright":    
+        img_bound_tr, x_starttr, y_starttr = extract_plate_side(img, bounding_box, 
+                                       int(bounding_box[0]*img.shape[1]*0.1), 
+                                       int(bounding_box[1]*img.shape[0]*0.1), 1)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh,
+                                               x_start, y_start)
+    
+    if method == "bottomright":    
+        img_bound_br, x_startbr, y_startbr = extract_plate_side(img, bounding_box, 
+                                       int(bounding_box[0]*img.shape[1]*0.1), 
+                                       int(bounding_box[1]*img.shape[0]*0.1),2)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh,
+                                               x_start, y_start)
+
+    if method == "bottomleft":
+        img_bound_bl, x_startbl, y_startbl = extract_plate_side(img, bounding_box, 
+                                       int(bounding_box[0]*img.shape[1]*0.1), 
+                                       int(bounding_box[1]*img.shape[0]*0.1), 3)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh,
+                                               x_start, y_start)
+    
+    if method == "topleft":
+        img_bound_tl, x_starttl, y_starttl = extract_plate_side(img, bounding_box, 
+                                       int(bounding_box[0]*img.shape[1]*0.1), 
+                                       int(bounding_box[1]*img.shape[0]*0.1), 4)
+        sorted_contours, gray, thresh = image_preproc(img_bound)
+        data_plate, plate_num = ocr_extraction(sorted_contours, gray, thresh, 
+                                               x_start, y_start)
+    
+    return data_plate, plate_num
+
+
+def ocr_validation(data_plate, plate_num, methods):
+    candidate = max(plate_num)
+    ind = methods[plate_num.index(candidate)]
+    
+    longest_frame = data_plate[ind]
+    
+    indices = longest_frame.index.tolist()
+    
+    for i in indices: 
+        width = int(longest_frame.width[i])
+        height = int(longest_frame.height[i])
+        conf = longest_frame.conf[i]
+        char = longest_frame.text[i]
+        
+        for m in methods: 
+            cur_frame = data_plate[m] 
+            if cur_frame.empty: continue
+            cur_opp = cur_frame[(cur_frame.width > width - 2) & (cur_frame.width < width + 2)]
+            cur_opp = cur_frame[(cur_frame.height > height - 2) & (cur_frame.height < height + 2)]
+            if cur_opp.empty: continue
+            indi = cur_opp.index.tolist()
+            if len(cur_opp.index) == 1:
+                if cur_opp.text[indi].to_string()[len(cur_opp.text[indi].to_string()) - 1] == char:
+                    continue
+                else: 
+                    if (cur_opp.conf > conf).bool():
+                        candidate = candidate.replace(char, cur_opp.text[indi].to_string()[len(cur_opp.text[indi].to_string()) - 1])
+                    else: 
+                        continue
+            else:
+                for j in indi:
+                    if cur_opp.conf[j] > conf:
+                        candidate = candidate.replace(char, cur_opp.text[j].to_string()[len(cur_opp.text[j].to_string()) - 1])
+                    else: continue                       
+                
+    return candidate
---
+++ src/character_recognition/ocr_pipeline_notWorking.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import pandas as pd
+import pytesseract
+from pathlib import Path
+import ocr_functions
+import matplotlib as plt
+
+# from license_plate_extraction.prediction import predict_bounding_box
+# from license_plate_extraction import data_reader
+# from license_plate_extraction import settings
+# from license_plate_extraction import preprocessing
+# from license_plate_extraction import visualization_tools
+
+###############################################################################
+
+
+def ocr_pipeline(img, bounding_box):
+    # Setze cmd auf das Verzeichnis, in dem auch Tesseract drin ist
+    TESSERACT_DIR = Path(__file__).parent / "bin"
+    pytesseract.pytesseract.tesseract_cmd = TESSERACT_DIR / "tesseract.exe"
+
+    methods = ["normal", "up", "down", "left", "right", "topright", "bottomright", 
+               "bottomleft", "topleft"]
+    plate_nums = []
+    confi_frame = pd.DataFrame()
+    
+    for m in methods: 
+        data_plate, plate_num = ocr_functions.ocr(img, bounding_box, m)
+        if plate_num is not None: plate_nums.append(plate_num)
+        if not data_plate.empty: confi_frame = pd.concat([confi_frame, data_plate])
+        
+    char = ocr_functions.ocr_validation(confi_frame, plate_nums, methods)
+    
+    return char
+
+
+if __name__ == "__main__":
+    # a = ocr_pipeline(
+    #         cv2.imread(
+    #         "G:\Statistik\FallstudienII\Projekt2\Code\Daten\eu_cars+lps/BS47040_car_eu.jpg"
+    #         ),
+    #         (92 / 600, 201 / 387, (229 - 201) / 387, (214 - 92) / 600),
+    #         )   
+    # print(a)
+    # a = ocr_pipeline(
+    #     cv2.imread(
+    #         "G:\Statistik\FallstudienII\Projekt2\Code\Daten\eu_cars+lps/W053011_car_eu.jpg"
+    #     ),
+    #     (391 / 600, 202 / 425, (522 - 391) / 600, (232 - 202) / 425),
+    #     # modifiziert (nach oben verschoben):
+    #     #(371 / 600, 192 / 425, (522 - 371) / 600, (242 - 192) / 425)
+    # )
+    # print(a)
+    a = ocr_pipeline(
+        cv2.imread(
+            "G:\Statistik\FallstudienII\Projekt2\Code\Daten\eu_cars+lps/BIMMIAN_car_eu.jpg"
+        ),
+        (104 / 711, 210 / 450, (609 - 104) / 711, (326 - 210) / 450),
+    )
+    print(a)
+
+# if __name__ == "__main__":
+#     image_dir_eu = settings.DATA_DIR / "eu_cars+lps"
+#     image_dir_no_labels = settings.DATA_DIR / "no_labels"
+
+#     # image_paths = data_reader.get_image_paths_from_directory(
+#     #     image_dir_eu, contains="_car_"
+#     # )
+#     image_paths = data_reader.get_image_paths_from_directory(image_dir_eu)
+
+#     # test_path = image_dir_eu / "BS47040_car_eu.jpg"
+#     # make_prediction(test_path)
+
+#     for cur_path in image_paths:
+#         image_tensor = data_reader.read_image_as_tensor(cur_path)
+#         predicted_bounding_box = predict_bounding_box(image_tensor)
+#         image_numpy = image_tensor.numpy()
+#         cur_prediction = ocr_pipeline(image_numpy, predicted_bounding_box)
+#         print(cur_prediction)
+
+#         cur_image_tensor = data_reader.read_image_as_tensor(cur_path)
+#         visualization_tools.show_image(
+#             img = cur_image_tensor, plate_text = cur_prediction
+#         )
+        
+        
+        
+        
+        
+        
+        
+        
