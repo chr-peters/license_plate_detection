365a8cf76603904ec1c0c440b9f67b3806703a83
cxan96 <c.p.xanten@gmail.com>
2021-02-16 10:37:05 +0100

    fixing import problems

--- src/character_recognition/ocr_pipeline.py
+++ src/character_recognition/ocr_pipeline.py
@@ -3,9 +3,10 @@ import cv2
 import numpy as np
 import pandas as pd
 import pytesseract
-import ocr_functions_notWorking as ocr_functions
+import ocr_functions
 
 ###############################################################################
+
 
 def ocr_pipeline(img, bounding_box):
     # Setze cmd auf das Verzeichnis, in dem auch Tesseract drin ist
@@ -23,7 +24,7 @@ def ocr_pipeline(img, bounding_box):
         "bottomleft",
         "topleft",
     ]
-    
+
     confi_frame = pd.DataFrame()
 
     for m in methods:
@@ -38,16 +39,16 @@ def ocr_pipeline(img, bounding_box):
 ###############################################################################
 
 if __name__ == "__main__":
-    from license_plate_extraction.prediction import predict_bounding_box
-    from license_plate_extraction import data_reader
-    from license_plate_extraction import settings
-    from license_plate_extraction import preprocessing
-    from license_plate_extraction import visualization_tools
+    # from license_plate_extraction.prediction import predict_bounding_box
+    # from license_plate_extraction import data_reader
+    # from license_plate_extraction import settings
+    # from license_plate_extraction import preprocessing
+    # from license_plate_extraction import visualization_tools
 
     image_dir_vali_eu = settings.DATA_DIR / "validation_eu"
     image_dir_vali_ro = settings.DATA_DIR / "validation_ro"
 
-    #image_paths = data_reader.get_image_paths_from_directory(image_dir_vali_eu)
+    # image_paths = data_reader.get_image_paths_from_directory(image_dir_vali_eu)
     image_paths = data_reader.get_image_paths_from_directory(image_dir_vali_ro)
 
     for cur_path in image_paths:
@@ -58,13 +59,11 @@ if __name__ == "__main__":
         print(cur_prediction)
 
         cur_image_tensor = data_reader.read_image_as_tensor(cur_path)
-        visualization_tools.show_image(
-            img = cur_image_tensor, plate_text = cur_prediction
-        )
-        
-    #data_dir = Path(__file__).parent.parent.parent / "data"
-    #a = ocr_pipeline(
+        visualization_tools.show_image(img=cur_image_tensor, plate_text=cur_prediction)
+
+    # data_dir = Path(__file__).parent.parent.parent / "data"
+    # a = ocr_pipeline(
     #        cv2.imread(str(data_dir / "eu_cars+lps" / "BIMMIAN_car_eu.jpg")),
     #        (104 / 711, 210 / 450, (609 - 104) / 711, (326 - 210) / 450),
     #     )
-    #print(a)
+    # print(a)
